2dacab0a49055791e9d8598f6930e10c40635526
fatGubari <fatimajubary@gmail.com>
2023-09-07 16:11:45 +0300

    update 2

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -44,7 +44,7 @@ const Contact = () => {
     <div className="h-full bg-primary/30 pt-5  mt-1 sm:mt-0">
       <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
         {/* text & form */}
-        <div className="flex flex-col w-full max-w-[700px] overflow-y-scroll scrollbar-thin scrollbar-dark md:overflow-y-hidden h-full">
+        <div className="flex flex-col w-full max-w-[700px]">
           {/* text */}
           <h2 className="h2 text-center mb-8">
             Lets <span className="text-accent"> connect. </span>
--- pages/work/index.js
+++ pages/work/index.js
@@ -7,13 +7,13 @@ const Work = () => {
     <div className="h-full bg-primary/30 py-24 flex items-center mt-4 sm:mt-0">
       <Circles />
       <div className="container mx-auto overflow-y-scroll scrollbar-thin scrollbar-dark md:overflow-y-hidden h-full md:h-auto">
-        <div className="flex flex-col xl:flex-row gap-x-8 mt-5">
+        <div className="flex flex-col xl:flex-row gap-x-8 mt-4">
           {/* text */}
-          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-3 xl:mb-0">
             <h2 className="h2 xl:mt-8">
               My Work<span className="text-cyan-500">.</span>
             </h2>
-            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">
+            <p className="mb-3 max-w-[400px] mx-auto lg:mx-0">
               I am passionate about building web applications that are both
               beautiful and functional. I have experience with a variety of
               technologies, including HTML, CSS, JavaScript, React and Node.js.
